790b62a53c2bacefbf66198b51255c5237c384a2
Claire Xenia Wolf <claire@clairexen.net>
2026-09-13 03:07:04 +0200

    update
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- fiddle.sh
+++ fiddle.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-[ $# -eq 0 ] && set help
+[ $# -eq 0 ] && set -- --help
 while [ $# -gt 0 ]; do
 	case "$1" in
 		--setxlog2)
 			sed -i -e "/parameter integer XLOG2 =/ s/[0-9]*,/$2,/" bm{sag,gas,waw,aww,ext,dep}.v
-			grep "parameter integer XLOG2 = [0-9]*," bm{sag,gas,waw,aww,ext,dep}.v
+			grep -E "parameter integer XLOG2 = [0-9]*," bm{sag,gas,waw,aww,ext,dep}.v
 			shift 2
 			;;
 		--enable-yices)
 			sed -i -e "/^#smtbmc yices/ s/^#//" prove_bm{sag,gas,waw,aww,ext,dep}.sby
-			grep "^#?smtbmc yices" prove_bm{sag,gas,waw,aww,ext,dep}.sby
+			grep -E "^#?smtbmc yices" prove_bm{sag,gas,waw,aww,ext,dep}.sby
 			shift
 			;;
 		--enable-boolector)
 			sed -i -e "/^#smtbmc boolector/ s/^#//" prove_bm{sag,gas,waw,aww,ext,dep}.sby
-			grep "^#?smtbmc boolector" prove_bm{sag,gas,waw,aww,ext,dep}.sby
+			grep -E "^#?smtbmc boolector" prove_bm{sag,gas,waw,aww,ext,dep}.sby
 			shift
 			;;
 		--disable-yices)
 			sed -i -e "/^smtbmc yices/ s/^/#/" prove_bm{sag,gas,waw,aww,ext,dep}.sby
-			grep "^#?smtbmc yices" prove_bm{sag,gas,waw,aww,ext,dep}.sby
+			grep -E "^#?smtbmc yices" prove_bm{sag,gas,waw,aww,ext,dep}.sby
 			shift
 			;;
 		--disable-boolector)
 			sed -i -e "/^smtbmc boolector/ s/^/#/" prove_bm{sag,gas,waw,aww,ext,dep}.sby
-			grep "^#?smtbmc boolector" prove_bm{sag,gas,waw,aww,ext,dep}.sby
+			grep -E "^#?smtbmc boolector" prove_bm{sag,gas,waw,aww,ext,dep}.sby
 			shift
 			;;
 		--reset)
+			( set -x
 			bash fiddle.sh --setxlog2 4
 			bash fiddle.sh --enable-yices
-			bash fiddle.sh --enable-boolector
+			bash fiddle.sh --enable-boolector; )
 			shift
 			;;
 		*)
